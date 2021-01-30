7375c6fab033b1b60d05363820990ef9d283b201
rutu <rutugaikwad843@gmail.com>
2021-01-30 23:39:25 +0530

    TODO 7.1: Implementation of APIs

--- src/main/java/com/upgrad/hirewheels/HireWheelsApplication.java
+++ src/main/java/com/upgrad/hirewheels/HireWheelsApplication.java
@@ -3,9 +3,11 @@ package com.upgrad.hirewheels;
 import com.upgrad.hirewheels.dao.*;
 import com.upgrad.hirewheels.entities.*;
 import com.upgrad.hirewheels.services.initService;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 
@@ -15,10 +17,15 @@ public class HireWheelsApplication {
 	public static void main(String[] args) {
 
 		ApplicationContext context = SpringApplication.run(HireWheelsApplication.class, args);
-		initService initService= context.getBean(initService.class);
-        initService.start();
+		//initService initService= context.getBean(initService.class);
+        //initService.start();
 
 
+
+	}
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
---
+++ src/main/java/com/upgrad/hirewheels/controllers/VehicleController.java
@@ -0,0 +1,58 @@
+package com.upgrad.hirewheels.controllers;
+
+import com.upgrad.hirewheels.dto.VehicleDto;
+import com.upgrad.hirewheels.entities.Vehicle;
+import com.upgrad.hirewheels.exceptions.VehicleDetailsNotFoundException;
+import com.upgrad.hirewheels.services.VehicleServiceImpl;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.chrono.ChronoLocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@RestController
+@RequestMapping(value="/hirewheels/v1/vehicles")
+public class VehicleController {
+
+        @Autowired
+        VehicleServiceImpl vehicleService;
+
+        @Autowired
+        ModelMapper modelmapper;
+
+        @GetMapping(value = "/data",produces= MediaType.APPLICATION_JSON_VALUE)
+        public ResponseEntity getVehicles(@QueryParam("categoryName") String categoryName,
+                                          @QueryParam("pickupDate") ChronoLocalDate pickupDate,
+                                          @QueryParam("dropoffDate") ChronoLocalDate dropoffDate,
+                                          @QueryParam("locationId") int locationId)
+                throws VehicleDetailsNotFoundException {
+
+
+                if (categoryName == null || pickupDate == null || dropoffDate == null || locationId <= 0) {
+                        List<Vehicle> vehicles = vehicleService.getAllVehicles();
+                        List<VehicleDto> vehicleDtos = new ArrayList<>();
+                        for (Vehicle vehicle : vehicles) {
+                                vehicleDtos.add(modelmapper.map(vehicle, VehicleDto.class));
+                        }
+
+                        return new ResponseEntity<>(vehicleDtos, HttpStatus.OK);
+                }
+                Set<Vehicle> vehicles = vehicleService.getAvailableVehicles(categoryName, locationId, pickupDate, dropoffDate);
+                List<VehicleDto> vehicleDtos = new ArrayList<>();
+                for (Vehicle vehicle : vehicles) {
+                        vehicleDtos.add(modelmapper.map(vehicle, VehicleDto.class));
+                }
+
+                return new ResponseEntity<>(vehicleDtos, HttpStatus.OK);
+
+
+        }
+}
