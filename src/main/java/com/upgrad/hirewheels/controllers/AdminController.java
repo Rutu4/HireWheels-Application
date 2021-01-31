ed7c2edba80613532e61a342855526a11d7114c0
rutu <rutugaikwad843@gmail.com>
2021-01-31 14:55:40 +0530

    TODO 9.1: Logging the messages of endpoints implemented in the AdminController class

--- src/main/java/com/upgrad/hirewheels/controllers/AdminController.java
+++ src/main/java/com/upgrad/hirewheels/controllers/AdminController.java
@@ -11,6 +11,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @RestController
 @RequestMapping(value="/hirewheels/v1")
 public class AdminController {
@@ -21,12 +24,14 @@ public class AdminController {
     @Autowired
     ModelMapper modelmapper;
 
+    private static final Logger logger = LoggerFactory.getLogger(AdminController.class);
 
     @PostMapping(value="/vehicles",consumes= MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity newBooking(@RequestBody VehicleDto vehicleDto) {
+    public ResponseEntity newVehicle(@RequestBody VehicleDto vehicleDto) {
         Vehicle newVehicle = modelmapper.map(vehicleDto, Vehicle.class);
         Vehicle savedVehicle = vehicleService.addVehicle(newVehicle);
         VehicleDto savedVehicleDto = modelmapper.map(savedVehicle, VehicleDto.class);
+        logger.debug("Accepted new vehicle details",savedVehicle);
         return new ResponseEntity<>(savedVehicleDto, HttpStatus.CREATED);
     }
     @PutMapping(value ="/vehicles/{id}",consumes =MediaType.APPLICATION_JSON_VALUE )
@@ -35,6 +40,7 @@ public class AdminController {
         Vehicle vehicle= vehicleService.getVehicleDetails(id);
         vehicle.setAvailabilityStatus(availabilityStatus);
         vehicleService.addVehicle(vehicle);
+        logger.debug("Update vehicle availability details : vehicle id :" + id, vehicle);
         return new ResponseEntity<>("saved",HttpStatus.OK);
     }
     }
